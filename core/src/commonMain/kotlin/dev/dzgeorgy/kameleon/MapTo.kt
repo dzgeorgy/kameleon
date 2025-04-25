41289e1cddc1862ab288ac6bf461b5054983c409
Georgy Dzutsev <dzgeorgy@icloud.com>
2025-04-25 14:34:56 +0300

    feat: add target parameter to MapTo annotation

--- core/src/commonMain/kotlin/dev/dzgeorgy/kameleon/MapTo.kt
+++ core/src/commonMain/kotlin/dev/dzgeorgy/kameleon/MapTo.kt
@@ -1,5 +1,9 @@
 package dev.dzgeorgy.kameleon
 
+import kotlin.reflect.KClass
+
 @Retention(AnnotationRetention.SOURCE)
 @Target(AnnotationTarget.CLASS)
-public annotation class MapTo()
+public annotation class MapTo(
+    val target: KClass<*>
+)
