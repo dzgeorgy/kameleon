008f3f35a1098f29036065453d323f2a91c5f4fc
Georgy Dzutsev <dzgeorgy@icloud.com>
2025-04-25 13:36:43 +0300

    init

---
+++ core/src/commonMain/kotlin/dev/dzgeorgy/kameleon/MapTo.kt
@@ -0,0 +1,5 @@
+package dev.dzgeorgy.kameleon
+
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.CLASS)
+public annotation class MapTo()
---
+++ sample/src/commonMain/kotlin/dev/dzgeorgy/kameleon/sample/Main.kt
@@ -0,0 +1,5 @@
+package dev.dzgeorgy.kameleon.sample
+
+fun main() {
+    println("Hello, World!")
+}
