c06f1f23b6dc3759bf0c1fedd9842df8e417cd61
Georgy Dzutsev <dzgeorgy@icloud.com>
2025-04-27 18:36:08 +0300

    feat: enhance MapTo annotation processing with KotlinPoet integration and add mapper generation

--- sample/src/commonMain/kotlin/dev/dzgeorgy/kameleon/sample/Main.kt
+++ sample/src/commonMain/kotlin/dev/dzgeorgy/kameleon/sample/Main.kt
@@ -1,5 +1,22 @@
 package dev.dzgeorgy.kameleon.sample
 
+import dev.dzgeorgy.kameleon.MapTo
+
 fun main() {
-    println("Hello, World!")
+    val track = Track(
+        title = "Blank Space",
+        length = 3.51f,
+    )
+    println(track.toTrackEntity())
 }
+
+@MapTo(TrackEntity::class)
+internal data class Track(
+    val title: String,
+    val length: Float
+)
+
+data class TrackEntity(
+    val title: String,
+    val length: Float
+)
